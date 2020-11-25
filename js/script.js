930a639caf43bb42e495586491762343efcc248b
Bigjames728 <bigjames728@gmail.com>
2020-11-24 16:33:29 -0800

    Added a change event listener to the jobRole variable
    Added a change event listener to the jobRole variable so that when a user selects a job from the drop down menu, it hides the "other-job-role input and if they select "other", it shows the other-job-role input box.

--- js/script.js
+++ js/script.js
@@ -2,3 +2,18 @@ let name = document.getElementById("name");
 
 name.focus();
 
+let jobRole = document.getElementById('title');
+let otherJobRole = document.getElementById('other-job-role');
+
+otherJobRole.style.display = 'none';
+
+jobRole.addEventListener('change', (e) => {
+    if ( e.target.value === 'other') {
+        otherJobRole.style.display = 'block';
+    } else {
+        otherJobRole.style.display = 'none';
+    }
+});
+
+console.log(jobRole);
+console.log(otherJobRole);
